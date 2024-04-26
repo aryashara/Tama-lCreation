2ac694bcc3615ae3cd7a2ff9b1349ed6eafa5291
aryashara <tanyavhb@gmail.com>
2024-04-26 16:12:28 +0200

    ok

--- SitePerso/src/app/components/main/main.component.ts
+++ SitePerso/src/app/components/main/main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
+
 export class MainComponent {
 
 }
